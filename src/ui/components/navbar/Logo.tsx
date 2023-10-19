2cf30fb35a494670afd0b47f36096c3779767f2c
charan379 <37008444+charan379@users.noreply.github.com>
2023-10-19 15:33:23 +0530

    minor tweaks

--- src/ui/components/navbar/Logo.tsx
+++ src/ui/components/navbar/Logo.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 export const Logo = () => {
+  console.log(process?.env?.NEXT_PUBLIC_BASE_URL)
   return (
     <a href="#" className="flex items-center">
-      <img
-        src="http://localhost:3000/favicon.ico"
-        className="h-8 mr-3"
-        alt="Flowbite Logo"
-      />
+      <img src={`${process?.env?.NEXT_PUBLIC_BASE_URL}favicon.ico`} className="h-8 mr-3" alt="Logo" />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
         Games Zone
       </span>
