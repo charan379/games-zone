576e1c5ce79cf98b09d8ee1d9128c02a8068e974
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-22 23:22:16 +0530

    minor tweaks

--- src/app/admin/games/page.tsx
+++ src/app/admin/games/page.tsx
@@ -1,14 +1,9 @@
-import SearchForm from "@/ui/features/games/Forms/SearchFrom";
-import GameCard from "@/ui/components/games/cards/GameCard";
-import AdminGames from "@/ui/components/games/views/AdminGames";
+import AdminGames from "@/ui/features/games/views/AdminGames";
 
 export default function Games() {
-
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-start p-2">
-
-            <AdminGames />
-
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start p-2">
+      <AdminGames />
+    </main>
+  );
 }
--- src/ui/features/login/Login.tsx
+++ src/ui/features/login/Login.tsx
@@ -58,9 +58,9 @@ const Login: React.FC = () => {
 
     return (
         <>
-            <div className="relative flex min-h-screen lg:min-h-max text-gray-800 antialiased flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+            <div className="relative flex min-h-screen lg:min-h-max text-gray-800 antialiased flex-col justify-center overflow-hidden py-6 sm:py-12">
                 <div className="relative py-3 sm:w-96 mx-auto text-center">
-                    <span className="text-2xl font-light ">Login to your account</span>
+                    <span className="text-2xl font-semibold">Login to your account</span>
                     <div className="mt-4 bg-white shadow-md rounded-lg text-left">
                         <div className="h-2 bg-slate-400 rounded-t-md"></div>
                         <form className="px-8 py-6 " onSubmit={handleSubmit}>
