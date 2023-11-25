8f9d335a469da87ed31bc30096d2735d5e5fdd43
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-25 14:20:36 +0530

    bug fixes

--- src/lib/utils/convertTo12HrFormat.ts
+++ src/lib/utils/convertTo12HrFormat.ts
@@ -1,15 +1,19 @@
 export default function convertTo12HourFormat(time24: string): string {
-    const [hours, minutes] = time24.split(':');
-    let period: 'AM' | 'PM' = 'AM';
+    try {
+        const [hours, minutes] = time24.split(':');
+        let period: 'AM' | 'PM' = 'AM';
 
-    let hours12 = parseInt(hours, 10);
+        let hours12 = parseInt(hours, 10);
 
-    if (hours12 >= 12) {
-        period = 'PM';
-        if (hours12 > 12) {
-            hours12 -= 12;
+        if (hours12 >= 12) {
+            period = 'PM';
+            if (hours12 > 12) {
+                hours12 -= 12;
+            }
         }
-    }
 
-    return `${hours12}:${minutes} ${period}`;
+        return `${hours12}:${minutes} ${period}`;
+    } catch (error) {
+        return 'error'
+    }
 }
