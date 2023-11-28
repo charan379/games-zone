cd6790cdd7dfe70877b92a0cbf174c0e2b1a5d27
charan379 <37008444+charan379@users.noreply.github.com>
2023-11-28 19:45:07 +0530

    minor tweaks

--- src/ui/modules/navbar/Menu.tsx
+++ src/ui/modules/navbar/Menu.tsx
@@ -14,41 +14,46 @@ const Menu = () => {
 
   const { data: session, status: authStatus } = useSession();
 
-  return (
-    <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-      {links.map((link) => {
-        return (
-          <li key={uuid4()}>
-            <MenuItem {...link} />
-          </li>
-        );
-      })}
 
-      {/* admin */}
-      {authStatus === "authenticated" &&
-        session.user.roles.includes("ROLE_ADMIN") ? (
-        <>
-          <li key={uuid4()}>
-            <MenuItem href="/admin" label="Admin Dashboard" />
-          </li>
-        </>
-      ) : (
-        ""
-      )}
-      {/* auth options */}
-      {authStatus === "authenticated" ? (
+  if (authStatus === "authenticated") {
+    return (
+      <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+        {links.map((link) => {
+          return (
+            <li key={uuid4()}>
+              <MenuItem {...link} />
+            </li>
+          );
+        })}
+
+        {/* admin */}
+        {session.user.roles.includes("ROLE_ADMIN") ? (
+          <>
+            <li key={uuid4()}>
+              <MenuItem href="/admin" label="Admin Dashboard" />
+            </li>
+          </>
+        ) : (
+          ""
+        )}
+        {/* auth options */}
         <li key={uuid4()}>
           <MenuItem>
             <button onClick={() => signOut()}>Sign Out</button>
           </MenuItem>
         </li>
-      ) : (
-        <li key={uuid4()}>
-          <MenuItem href="/login" label="Sign In" />
-        </li>
-      )}
+      </ul>
+    )
+  }
+
+  return (
+    <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+      <li key={uuid4()}>
+        <MenuItem href="/login" label="Sign In" />
+      </li>
     </ul>
-  );
+  )
+
 };
 
 export default Menu;
